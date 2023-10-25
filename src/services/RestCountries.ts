55a292ba21aeec50db43111a3ae0c600eab4d7ba
girumel <girumel@email.com>
2023-10-25 23:18:17 +0300

    Update api service

--- src/services/RestCountries.ts
+++ src/services/RestCountries.ts
@@ -5,12 +5,12 @@ const API_URL = 'https://restcountries.com/v3.1/name/';
 export const getCountry = async (countryName: string) => {
   try {
     const response = await axios.get(`${API_URL}${countryName}`);
-    if (response.status === 200) {
-      return response.data;
-    } else if (response.status === 404) {
+      if (response.status === 200) {
+        return response.data;
+      }
+  } catch (error: any) {
+    if (error.response.status === 404) {
       return null;
     }
-  } catch (error) {
-    console.log(error);
   }
 }
